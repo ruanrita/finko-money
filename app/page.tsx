abff421711d035a0271fb20b7d5ebea0e07aa9e9
RuanRita <ruan.ritah@gmail.com>
2025-11-23 15:39:55 -0300

    update: change text on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
             </Link>
             <Link href="/signup">
               <Button className="btn-brand">
-                Começar Grátis
+                Começar
                 <ArrowRight className="ml-2 h-4 w-4" />
               </Button>
             </Link>
